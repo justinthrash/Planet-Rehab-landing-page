65de6568de8ee6f0caa5daa6f1080799c6667686
Justin Thrash <justinthrash@att.net>
2016-11-28 13:48:13 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {;
-  var moneyRaised = 2500; //Store amount raised here as a plain number, no $ or ,
+  var moneyRaised = 0; //Store amount raised here as a plain number, no $ or ,
   var moneyRaisedRounded = Math.round(moneyRaised);
   var percentRaised = (moneyRaised/250);
   var moneyRaisedText = '$' + moneyRaisedRounded;
