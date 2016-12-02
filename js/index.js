2be63f7d93d6db2665f15e90ab15aa281facfee1
Justin Thrash <justinthrash@att.net>
2016-12-02 08:27:12 -0600

    Status change

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {;
-  var moneyRaised = 1241; //Store amount raised here as a plain number, no $ or ,
+  var moneyRaised = 1551; //Store amount raised here as a plain number, no $ or ,
                               //Type commit to update site
   var moneyRaisedRounded = Math.round(moneyRaised);
   var percentRaised = (moneyRaised/250);
